246d75c20b8444ca6c6be5144a1f3c15872cc283
Osman Bin Nasir <osmanbinnasir19@gmail.com>
2026-05-28 20:30:48 +0530

    check lint - III

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,23 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import security from "eslint-plugin-security";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js,
+      security
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      ...security.configs.recommended.rules
+    }
+  },
+  tseslint.configs.recommended
 ]);
